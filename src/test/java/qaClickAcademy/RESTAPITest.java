2d82e5cdbacac62e3115ef364eb46f7d3feddcab
Sagar <sagarpant56@gmail.com>
2020-07-15 19:05:43 +0530

    This is a commit from an Indian

--- src/test/java/qaClickAcademy/RESTAPITest.java
+++ src/test/java/qaClickAcademy/RESTAPITest.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 
 public class RESTAPITest {
     @Test
-    public void postJira()
-    {
-        System.out.println("Post Jira");
+    public void postJira() {
+        System.out.println("Post Jira 1");
+        System.out.println("Post Jira 2");
+        System.out.println("Post Jira 3");
     }
 
     @Test
-    public void deleteTwitter()
-    {
+    public void deleteTwitter() {
         System.out.println("Delete Twitter");
     }
 }
